6482b14f6c1642ced903350d2ac0154b7074cfcd
Vesorad <Vesorad@users.noreply.github.com>
2021-03-03 10:43:43 +0100

    save

--- GDS_Projekt_02/Assets/ScorePanelControll.cs
+++ GDS_Projekt_02/Assets/ScorePanelControll.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ScorePanelControll
+public class ScorePanelControll : MonoBehaviour
 {
     public Text name;
 
