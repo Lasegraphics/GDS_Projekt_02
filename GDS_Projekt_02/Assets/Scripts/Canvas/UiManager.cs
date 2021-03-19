17537b8af1c79019ce60fd6c4c3ff2d4781f5be3
Vesorad <57622258+Vesorad@users.noreply.github.com>
2021-03-19 10:33:41 +0100

    -Poprawa Ui
    panele juz się zmieniają

--- GDS_Projekt_02/Assets/Scripts/Canvas/UiManager.cs
+++ GDS_Projekt_02/Assets/Scripts/Canvas/UiManager.cs
@@ -35,19 +35,22 @@ public class UiManager : MonoBehaviour
         endRoundText.SetBool("Out", true);
         CloseEnemyScorePanel();
         CloseScorePanel();
+
+        orangePanel.GetComponent<RectTransform>().anchoredPosition = new Vector2(-364, 392);
+        bluePanel.GetComponent<RectTransform>().anchoredPosition = new Vector2(477, 392);
     }
     public void ChangeTurnUi(int player)
     {
         currentPlayer = player;
         if (player == 0)
         {
-            orangePanel.GetComponent<RectTransform>().anchoredPosition = new Vector2(477, 392);
-            bluePanel.GetComponent<RectTransform>().anchoredPosition = new Vector2(-364, 392);
+            orangePanel.GetComponent<RectTransform>().anchoredPosition = new Vector2(-364, 392);
+            bluePanel.GetComponent<RectTransform>().anchoredPosition = new Vector2(477, 392);
         }
         else
         {
-            orangePanel.GetComponent<RectTransform>().anchoredPosition = new Vector2(-364, 392);
-            bluePanel.GetComponent<RectTransform>().anchoredPosition = new Vector2(477, 392);
+            orangePanel.GetComponent<RectTransform>().anchoredPosition = new Vector2(477, 392);
+            bluePanel.GetComponent<RectTransform>().anchoredPosition = new Vector2(-364, 392);
         }
         endRoundText.SetBool("Out", false);
         endRoundText.GetComponent<Text>().text = "KONIEC TURY GRACZA :" + player;
