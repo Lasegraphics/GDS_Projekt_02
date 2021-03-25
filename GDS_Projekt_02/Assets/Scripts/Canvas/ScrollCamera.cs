92966b403cc959b77d424eed464ca502d48ecc18
Vesorad <57622258+Vesorad@users.noreply.github.com>
2021-03-25 12:03:41 +0100

    -Dodanie zmiany camery

--- GDS_Projekt_02/Assets/Scripts/Canvas/ScrollCamera.cs
+++ GDS_Projekt_02/Assets/Scripts/Canvas/ScrollCamera.cs
@@ -4,89 +4,42 @@ using UnityEngine;
 
 public class ScrollCamera : MonoBehaviour
 {
-    [Header("Pozycja kamery")]
+    [Header("Pozycja pocz¹tkowa kamery")]
     public int startPosCam;
     public int normalPosCam;
     public float sppedChangeCamera;
 
     [Header("Poruszanie siê kamery")]
-    public float dragSpeed = 2;
-    public float outerLeft = -10f;
-    public float outerRight = 10f;
-   
-    private Vector3 dragOrigin;
-    bool cameraDragging = true;
+    [SerializeField] float movmentSpeed;
+    [SerializeField] float minimumYValue;
+    [SerializeField] float maximumYValue;
+
     Camera posCamera;
-    UiManager uiManager;
     float timeToScroll;
+
     private void Awake()
     {
         posCamera = GetComponent<Camera>();
         posCamera.orthographicSize = startPosCam;
-        uiManager = FindObjectOfType<UiManager>();
     }
     void Update()
     {
-        if (uiManager.isStart == false)
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f)
         {
-            MoveCameraToNormalPos();
+            if (transform.position.y < maximumYValue)
+            {
+                var newPos = Vector3.up * movmentSpeed * Time.deltaTime;
+                transform.Translate(newPos, Space.World);
+            }
         }
-        Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-        float left = Screen.width * 0.2f;
-        float right = Screen.width - (Screen.width * 0.2f);
-        if (mousePosition.x < left)
+        if (Input.GetAxis("Mouse ScrollWheel") < 0f)
         {
-            cameraDragging = true;
-        }
-        if (mousePosition.x > right)
-        {
-            cameraDragging = true;
-        }
-        if (cameraDragging)
-        {
-
-            if (Input.GetMouseButtonDown(2))
+            if (transform.position.y > minimumYValue)
             {
-                dragOrigin = Input.mousePosition;
-                return;
+                var newPos = Vector3.down * movmentSpeed * Time.deltaTime;
+                transform.Translate(newPos, Space.World);
             }
 
-            if (!Input.GetMouseButton(2)) return;
-
-            Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
-            Vector3 move = new Vector3(pos.x * dragSpeed, 0, 0);
-
-            if (move.x > 0f)
-            {
-                if (this.transform.position.x < outerRight)
-                {
-                    transform.Translate(move, Space.World);
-                }
-            }
-            else
-            {
-                if (this.transform.position.x > outerLeft)
-                {
-                    transform.Translate(move, Space.World);
-                }
-            }
-
-            Vector3 move2 = new Vector3(0, pos.y * dragSpeed, 0);
-
-            if (move2.y > 0f)
-            {
-                if (this.transform.position.y < outerRight)
-                {
-                    transform.Translate(move2, Space.World);
-                }
-            }
-            else
-            {
-                if (this.transform.position.y > outerLeft)
-                {
-                    transform.Translate(move2, Space.World);
-                }
-            }
         }
 
     }
@@ -97,6 +50,6 @@ public class ScrollCamera : MonoBehaviour
             timeToScroll += Time.deltaTime;
             posCamera.orthographicSize = Mathf.SmoothStep(startPosCam, normalPosCam, timeToScroll);
         }
-       
     }
+
 }
