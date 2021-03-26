7581b1207ae98173dd17490df1841e22285a7c30
Vesorad <57622258+Vesorad@users.noreply.github.com>
2021-03-26 23:45:02 +0100

    Merge branch 'main' into zapis-maina-dziala

--- GDS_Projekt_02/Assets/MoveToMousePosCanvas.cs
+++ GDS_Projekt_02/Assets/MoveToMousePosCanvas.cs
@@ -7,12 +7,26 @@ public class MoveToMousePosCanvas : MonoBehaviour
 {
    public Canvas myCanvas;
     public int changeX;
+    public Text text;
 
-    void Start()
+   public void UpdatePos(int area )
     {
-    }
-   public void UpdatePos( )
-    {
+        switch (area)
+        {
+            case 0:
+                text.text = "X damage at the beginning of next turn";
+                break;
+            case 1:
+                text.text = "X% chance to dodge";
+                break;
+            case 2:
+                text.text = "Heal X at the end of current turn";
+                break;
+            default:
+                break;
+        }
+
+
         Vector2 pos;
         RectTransformUtility.ScreenPointToLocalPointInRectangle(myCanvas.transform as RectTransform, Input.mousePosition, myCanvas.worldCamera, out pos);
         transform.position = myCanvas.transform.TransformPoint(pos.x+ changeX, pos.y, 0);
