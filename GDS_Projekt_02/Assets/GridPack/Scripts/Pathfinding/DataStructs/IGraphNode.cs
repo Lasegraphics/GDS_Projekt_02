4480717d85e7dbda107a4774bb1149fa08f5225b
Vesorad <57622258+Vesorad@users.noreply.github.com>
2021-04-01 20:32:55 +0200

    prefab

--- GDS_Projekt_02/Assets/GridPack/Scripts/Pathfinding/DataStructs/IGraphNode.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GridPack.Pathfinding.DataStructs
-{
-    public interface IGraphNode
-    {
-        //Metoda zwraca dystans do IGraphNode który jest podany jako parametr
-        int GetDistance(IGraphNode other);
-    }
-}
-
