0992eb3f291bc1f85162e2c00546195e3eb8546b
Lasegraphics <75523524+Lasegraphics@users.noreply.github.com>
2021-04-03 18:31:02 +0200

    UP

--- GDS_Projekt_02/Assets/Scripts/GridPack/SceneScripts/ArmoredEntity.cs
+++ GDS_Projekt_02/Assets/Scripts/GridPack/SceneScripts/ArmoredEntity.cs
@@ -4,16 +4,14 @@ using GridPack.Units;
 using UnityEngine;
 using System; 
 using System.Threading; 
-using Random = UnityEngine.Random;
+//using Random = UnityEngine.Random;
 
 namespace GridPack.SceneScripts
 {
-
     public class ArmoredEntity : Entity
     {
         public override event EventHandler<AttackEventArgs> UnitAttacked;
         public override event EventHandler<AttackEventArgs> UnitDestroyed;
-
         protected static bool IsIgnored;
         public override void Initialize()
         {
@@ -21,27 +19,6 @@ namespace GridPack.SceneScripts
             TotalArmorPoints = ArmorPoints;
             IsIgnored = false;
         }
-
-
-        /*  MarkAsDefending(aggressor); 
-          int damageTaken = Defend(aggressor, damage);
-          damageTaken = Random.Range(attackMin,attackMax);
-
-
-
-
-          if(HitPoints <= 0)
-          {
-              if (UnitDestroyed != null)
-              {
-                  UnitDestroyed.Invoke(this, new AttackEventArgs(aggressor, this, damage));
-              }
-              OnDestroyed();
-          }
-          Debug.Log("Obecne Zdrowie: " + HitPoints + " Zadane Obrazenia: " + damageTaken);
-          */
     }
-
-
 }
 
